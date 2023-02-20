0f59195ff627892f0c12979b5fdeec82fd76c35e
tsaebst <125909246+tsaebst@users.noreply.github.com>
2023-02-20 20:32:12 +0200

    Yay

--- Pattern2.py
+++ Pattern2.py
@@ -9,28 +9,32 @@ i=0
 processed_chars = ""
 while i < n1 and result:
     #Take i-character from input-line-1 and input-line-2
-    ln1_char = #get i-character from ln1
-    ln2_char = #get i-character from ln2
+    ln1_char = ln1[i] #get i-character from ln1
+    ln2_char = ln2[i]#get i-character from ln2
 
     #Check, that "processed_chars" contains neither of characters above (so, non of these characters have already been processed inside this loop)
-    is_ln1_char_processed = #check processed_chars does not contain character[i] from line 1
-    is_ln2_char_processed = #check processed_chars does not contain character[i] from line 2
+    is_ln1_char_processed = ln1_char.__contains__(ln1_char)#check processed_chars does not contain character[i] from line 1
+    is_ln2_char_processed = ln2_char.__contains__(ln2_char)#check processed_chars does not contain character[i] from line 2
 
     if not(is_ln1_char_processed) and not(is_ln2_char_processed):
         print("Both characters not processed yet, let's check them")
         #Because we'are going to process i-characters from both input strings - add them to the string which contains already processed characters
-        processed_chars += #...
+        processed_chars += ln1_char + ln2_char#...
 
         #Lists to store indexes (positions) of i-character in input line1 and input line2.
         #When you declare lists, it would make sense to add "i" as first index in the list by default (because here we ARE processing i-character, so need to store its index in this list)
-        ln1_char_indexes = #...
-        ln2_char_indexes = #...
+        ln1_char_indexes = [i]#...
+        ln2_char_indexes = [i]#...
 
         #Have inner-loop to search for i-character in the remaining part of input-line1 and input-line 2
         j = i + 1
         while j < n1:
             #if j-character from line 1 equals to i-character from line 1, save it's index (which equals to "j") into the list of character indexes for input-line1
+            if ln1[i] == ln1[j]:
+                ln1_char_indexes.append(j)
             #if j-character from line 2 equals to i-character from line 1, save it's index (which equals to "j") into the list of character indexes for input-line2
+            if ln2[i] == ln2[j]:
+                ln2_char_indexes.append(j)
             j += 1
 
         print("List of indexes of line1 character", ln1_char, ":", ln1_char_indexes)
@@ -38,14 +42,14 @@ while i < n1 and result:
         print("Compare indexes of chars", ln1_char, "and", ln2_char, "inside their strings")
 
         #result = <result of comparing lists of indexes of i-character in line 1 and line 2 (ln1_char_indexes  vs. ln2_char_indexes)
-        result = #...
+        result = ln1_char_indexes == ln2_char_indexes#...
 
     elif is_ln1_char_processed and not(is_ln2_char_processed):
         #print("Only character from 1st line already processed. Comparison fails")
-        result = #Think what result should be equal to, if i-character already was processed before, but j-character was not processed yet?
+        result = False #Think what result should be equal to, if i-character already was processed before, but j-character was not processed yet?
     elif not(is_ln1_char_processed) and is_ln2_char_processed:
         #print("Only character from 2nd line already processed. Comparison fails")
-        result = #Think what result should be equal to, if i-character has not been processed before, but j-character has already been processed?
+        result = False#Think what result should be equal to, if i-character has not been processed before, but j-character has already been processed?
     else:
         print("Both characters already processed, which is OK. Do nothing at this iteration")
      
